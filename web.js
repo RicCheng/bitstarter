875230e872c777420dd4500b9e0c354e78d77d9c
Ubuntu <hoyu.cheng@gmail.com>
2013-07-19 15:36:45 +0000

    modified buffer line

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("fs.readFileSync('index.html'));
+  var buffer = new Buffer(16);
+  buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
